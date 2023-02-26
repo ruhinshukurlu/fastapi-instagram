424c57ec85af9f1a0906a7404f42a1d9416f1b76
Ruhin Shukurlu <ruhinshukurlu@gmail.com>
2023-02-26 19:31:47 +0400

    user create

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
 from db.database import engine
 from db import models
-
+from routers import user
 
 app = FastAPI()
 
+app.include_router(user.router)
 
-@app.get("/")
-def hello():
-    return "Hello world"
 
 
 models.Base.metadata.create_all(engine)
