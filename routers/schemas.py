9ff504d853082082d42d754c82f3afd049b09939
Ruhin Shukurlu <ruhinshukurlu@gmail.com>
2023-02-26 22:23:33 +0400

    post create endpoint

--- routers/schemas.py
+++ routers/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-
+from datetime import datetime
 
 class UserBase(BaseModel):
     username: str
@@ -10,5 +10,27 @@ class UserBase(BaseModel):
 class UserDisplay(BaseModel):
     username: str
     email: str
+    class Config():
+        orm_mode=True
+
+
+class User(BaseModel):
+    username:str
+    class Config():
+        orm_mode = True
+
+class PostBase(BaseModel):
+    image_url: str
+    image_url_type: str
+    caption: str
+    creator_id: int
+
+
+class PostDisplay(BaseModel):
+    image_url: str
+    image_url_type: str
+    caption: str
+    timestamp: datetime
+    user: User
     class Config():
         orm_mode=True
