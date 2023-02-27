9172d1da663b867f8613255012ba048e67f36bee
Ruhin Shukurlu <ruhinshukurlu@gmail.com>
2023-02-27 23:55:45 +0400

    auth

--- db/db_user.py
+++ db/db_user.py
@@ -2,7 +2,8 @@ from sqlalchemy.orm.session import Session
 from routers.schemas import UserBase
 from db.models import DbUser
 from db.hashing import Hash
-
+from fastapi.exceptions import HTTPException
+from fastapi import status
 
 def create_user(db: Session, request: UserBase):
     new_user = DbUser(
@@ -14,3 +15,10 @@ def create_user(db: Session, request: UserBase):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+def get_user_by_username(db:Session, username:str):
+    user = db.query(DbUser).filter(DbUser.username == username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with username {username} not found")
+    return user
